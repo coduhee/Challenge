f818f5bd86eeff97a447ce43dad803ea32a98545
coduhee <vdyjb@ewha.ac.kr>
2026-03-12 11:34:44 +0900

    ✨feat: 앱 전체에서 공통으로 쓸 모델 ContentItem 생성

---
+++ Challenge/Challenge/Sources/Domain/ContentItem.swift
@@ -0,0 +1,24 @@
+//
+//  ContentItem.swift
+//  Challenge
+//
+//  Created by 김주희 on 3/12/26.
+//
+
+import Foundation
+
+// MARK: 앱 전체에서 공통으로 쓸 모델
+struct ContentItem {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let imageURL: String
+    let description: String
+    let mediaType: MediaType
+}
+
+enum MediaType {
+    case music
+    case movie
+    case podcast
+}
