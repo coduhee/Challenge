b981169c5e2cb1e6089c46e9fbc7e0301455f623
coduhee <vdyjb@ewha.ac.kr>
2026-03-12 11:35:10 +0900

    ✨feat: 홈 화면 섹션 모델 HomeSection 생성

---
+++ Challenge/Challenge/Sources/Domain/HomeSection.swift
@@ -0,0 +1,14 @@
+//
+//  HomeSection.swift
+//  Challenge
+//
+//  Created by 김주희 on 3/12/26.
+//
+
+import Foundation
+
+// MARK: 홈 화면 섹션 모델
+struct HomeSection {
+    let title: String
+    let items: [ContentItem]
+}
