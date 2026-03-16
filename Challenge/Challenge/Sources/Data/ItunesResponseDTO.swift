fc909105943f2b4bba7151e719965fd6efe3dd9a
coduhee <vdyjb@ewha.ac.kr>
2026-03-17 00:23:11 +0900

    ♻️Refactor: 300x300으로 이미지 크기 조정

--- Challenge/Challenge/Sources/Data/ItunesResponseDTO.swift
+++ Challenge/Challenge/Sources/Data/ItunesResponseDTO.swift
@@ -9,8 +9,8 @@ import Foundation
 
 
 struct ItunesResponseDTO: Decodable {
-    let resultCount: Int // ex: 30개
-    let results: [ItunesItemDTO] // ex: 노래 데이터 30개 뭉치 배열
+    let resultCount: Int // ex: 10개
+    let results: [ItunesItemDTO] // ex: 노래 데이터 10개 뭉치 배열
 }
 
 struct ItunesItemDTO: Decodable {
@@ -19,10 +19,10 @@ struct ItunesItemDTO: Decodable {
     let trackName: String?
     let collectionName: String?
     let artistName: String?
-    let artworkUrl100: String?
     let artworkUrl60: String?
-    var artworkUrl200: String? {
-        return artworkUrl100?.replacingOccurrences(of: "100x100", with: "200x200")
+    let artworkUrl100: String?
+    var artworkUrl300: String? {
+        return artworkUrl100?.replacingOccurrences(of: "100x100", with: "300x300")
     }
     let longDescription: String?
     let shortDescription: String?
@@ -37,7 +37,7 @@ extension ItunesItemDTO {
             id: trackId ?? collectionId ?? 0,
             title: trackName ?? collectionName ?? "제목 없음",
             subtitle: artistName ?? "아티스트 없음",
-            imageURL: artworkUrl200 ?? artworkUrl100 ?? artworkUrl60 ?? "",
+            imageURL: artworkUrl300 ?? artworkUrl100 ?? artworkUrl60 ?? "",
             description: longDescription ?? shortDescription ?? primaryGenreName ?? "설명 없음",
             mediaType: mediaType
         )
