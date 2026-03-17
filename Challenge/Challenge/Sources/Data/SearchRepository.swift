90527238717ddd687f1942a7582415e31281a940
coduhee <vdyjb@ewha.ac.kr>
2026-03-17 09:45:10 +0900

    ♻️Refactor: Error 타입 정의

--- Challenge/Challenge/Sources/Data/SearchRepository.swift
+++ Challenge/Challenge/Sources/Data/SearchRepository.swift
@@ -10,28 +10,10 @@ import RxSwift
 
 final class SearchRepository: SearchRepositoryType {
     
-    private let networkManager: NetworkManager
-    
-    
-    init(networkManager: NetworkManager) {
-        self.networkManager = networkManager
+    // MARK: error
+    enum NetworkError: Error {
+        case invalidURL
     }
-    
-    
-//    // MARK: - fetchMusic
-//    func fetchMusic(term: String) -> Single<[ContentItem]> {
-//        // API Endpoint 이용해서 url 생성하기
-//        guard let url = APIEndpoint.search(term: term, media: "music") else {
-//            return .error(NSError(domain: "InvalidURL", code: -1))
-//        }
-//        
-//        // NetworkManager로 요청을 보내 데이터를 받아옴
-//        return networkManager.request(url)
-//            .map { (response: ItunesResponseDTO) in
-//                // DTO -> entity로 바꿔줌
-//                response.results.map { $0.toEntity(mediaType: .music) }
-//            }
-//    }
     
     
     // MARK: - fetchContent
@@ -46,13 +28,14 @@ final class SearchRepository: SearchRepositoryType {
         case .podcast:
             mediaString = "podcast"
         }
+
         
         guard let url = APIEndpoint.search(term: term, media: mediaString) else {
-            return .error(NSError(domain: "InvalidURL", code: -1))
+            return .error(NetworkError.invalidURL)
         }
         
         // NetworkManager로 요청을 보내 데이터를 받아옴
-        return networkManager.request(url)
+        return NetworkManager.shared.request(url)
             .map { (response: ItunesResponseDTO) in
                 // DTO -> entity로 바꿔줌
                 response.results.map { $0.toEntity(mediaType: mediaType) }
